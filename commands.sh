80068fce18de8408fb374275400f98394ee142b7
rafaelfbastos <rafaelfbastos@gmail.com>
2023-10-10 22:19:08 -0300

    15

--- commands.sh
+++ commands.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-
 python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py createsu
+python createsu.py
 gunicorn project.wsgi 
  
--- createsu.py
+++ createsu.py
@@ -1,14 +1,21 @@
-from django.contrib.auth.models import User
-from django.core.management.base import BaseCommand
+# script_criar_superusuario.py
 import os
+from django.contrib.auth.models import User
 
-class Command(BaseCommand):
-    help = 'Creates a superuser.'
+def criar_superusuario():
+    username = os.getenv('SUPER_USER', 'change-me'),
+    email = os.getenv('SUPER_USER_EMAIL', 'change-me')
+    password = os.getenv('SUPER_USER_PASSWORD', 'change-me')
 
-    def handle(self, *args, **options):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username=os.getenv('SUPER_USER', 'change-me'),
-                password=os.getenv('SUPER_USER_PASSWORD', 'change-me')
-            )
-        print('Superuser has been created.')
+    # Verifique se o superusuário já existe
+    if not User.objects.filter(username=username).exists():
+        # Crie o superusuário
+        User.objects.create_superuser(username, email, password)
+        print(f'Superusuário {username} criado com sucesso!')
+    else:
+        print(f'Superusuário {username} já existe.')
+
+if __name__ == '__main__':
+    criar_superusuario()
+
+
