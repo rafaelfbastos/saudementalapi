f6e13b4b3b542d411339d0def6177ff8e6072faf
rafaelfbastos <rafaelfbastos@gmail.com>
2023-10-10 22:48:31 -0300

    superuser script

--- core/management/commands/createsu.py
+++ core/management/commands/createsu.py
@@ -1,21 +1,16 @@
-# script_criar_superusuario.py
-import os
 from django.contrib.auth.models import User
-
-def criar_superusuario():
-    username = os.getenv('SUPER_USER', 'change-me'),
-    email = os.getenv('SUPER_USER_EMAIL', 'change-me')
-    password = os.getenv('SUPER_USER_PASSWORD', 'change-me')
-
-    # Verifique se o superusuário já existe
-    if not User.objects.filter(username=username).exists():
-        # Crie o superusuário
-        User.objects.create_superuser(username, email, password)
-        print(f'Superusuário {username} criado com sucesso!')
-    else:
-        print(f'Superusuário {username} já existe.')
-
-if __name__ == '__main__':
-    criar_superusuario()
+from django.core.management.base import BaseCommand
+import os
 
 
+class Command(BaseCommand):
+    help = 'Creates a superuser.'
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username=os.getenv('SUPER_USER', 'change-me'),
+                password=os.getenv('SUPER_USER_PASSWORD', 'change-me'),
+                email=os.getenv('SUPER_USER_EMAIL', 'change-me')
+            )
+        print('Superuser has been created.')
